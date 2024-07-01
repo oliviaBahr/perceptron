a5b614c48d289d017a4f21ed2156f2308e9e5990
Olivia Bahr <ob.bahr@gmail.com>
2024-07-01 14:30:15 -0600

    Add data_opts and .plot

--- src/perceptron.py
+++ src/perceptron.py
@@ -1,7 +1,11 @@
-from typing import Tuple
+import warnings
+import matplotlib.pyplot as plt
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from typing import Tuple, Literal, Iterator
 from scipy.sparse import spmatrix
 from functools import lru_cache
 from itertools import cycle
+from more_itertools import windowed
 from codetiming import Timer
 from numpy import ndarray, array_split, unique, zeros, array
 from sklearn.linear_model import Perceptron as skPerc
@@ -10,20 +14,23 @@ from sklearn.utils import shuffle
 from sklearn.datasets import load_svmlight_file
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.model_selection import train_test_split
+from sklearn.exceptions import ConvergenceWarning
+
+warnings.filterwarnings("ignore", category=ConvergenceWarning)
 
 
 class Loader:
-    @classmethod
+    @staticmethod
     def split(data, test_size=0.2) -> tuple[tuple, tuple]:
         X, tX, y, ty = train_test_split(data[0], data[1], test_size=test_size)
         return (X, y), (tX, ty)
 
-    @classmethod
+    @staticmethod
     @lru_cache
     def load_svm(pathname) -> tuple[spmatrix, ndarray]:
         return load_svmlight_file(pathname)
     
-    @classmethod
+    @staticmethod
     @lru_cache
     def load_imdb_binary(pathname):
         lines = [line.strip().split() for line in open(pathname)]
@@ -48,6 +55,9 @@ class Loader:
 
 class Perceptron:
     def __init__(self, dataset_name: str, traindata: Tuple[spmatrix | ndarray], testdata: Tuple[spmatrix | ndarray]) -> None:
+        # base sklearn perceptron
+        self.base = skPerc()
+
         # data
         self.X, self.y = traindata
         self.tX, self.ty = testdata
@@ -62,78 +72,97 @@ class Perceptron:
 
         # training run info
         self.accuracies = []
-        self.epochs = None
-        self.cycle = None
+        self.epoch_size = None
         self.ensemble_size = None
+        self.data_opts = None
         self.train_time = None
 
-    def test(self, clf) -> float:
-        return accuracy_score(self.ty, clf.predict(self.tX))
+    
+    def reset_base(self) -> None:
+        self.base = skPerc()
+        self.base.coef_ = zeros((self.n_weights, self.n_features))
+        self.base.intercept_ = zeros(self.n_weights)
+        self.accuracies = []
 
-    def train(self, ensemble_size=50, epochs=1, cycle_groups=None, log=True) -> None:
+    def test(self) -> float:
+        return accuracy_score(self.ty, self.base.predict(self.tX))
+
+    def train(self, ensemble_size=50, epoch_size=1.0, data_opts: Literal["partial", "cycle", "window"]=None, log=True) -> None:
         """
         Trains the perceptron model.
 
         Args:
-            `ensemble_size (int, optional)`: The number of perceptrons (weak learners) in the ensemble.
-            `epochs (int, optional)`: The number of times each learner iteratesover the training data. < 1 trains on only part of the data.
-            `cycle_groups (bool, optional)`: (if epochs < 1) Whether to cycle through the data groups or only use the first group.
-            `log (bool, optional)`: Print accuracies during training.
+            ensemble_size (int, optional):
+                The number of weak learners in the ensemble. Defaults to 50.
 
-        Raises:
-            AssertionError: epochs > 0
-            AssertionError: ensemble_size > 0 and isinstance(ensemble_size, int)
+            epoch_size (float, optional):
+                The fraction of the training data to use in each epoch. Defaults to 1.0.
+
+            data_opts (Literal["partial", "cycle", "window"], optional):
+                The data sampling option.
+                `"partial"`: Uses a partial subset of the training data in each epoch.
+                `"cycle"`: Cycles through different subsets of the training data in each epoch.
+                `"window"`: Uses a sliding window approach to sample the training data in each epoch.
+                `None`: Uses the entire training dataset in each epoch.
+
+            log (bool, optional):
+                Whether to log the accuracy during training. Defaults to True.
 
         Returns:
             None
         """
-        
-        assert (epochs > 0), "epochs must be greater than 0"
+        assert (epoch_size > 0), "epoch_size must be greater than 0"
         assert (ensemble_size > 0 and isinstance(ensemble_size, int)), "ensemble_size must be a positive integer"
         
-        # scikit shuffle
-        X, y = shuffle(self.X, self.y)
+        def data_generator() -> Iterator:
+            X, y = shuffle(self.X, self.y)
+            split_size = round(epoch_size*self.train_size)
+            n_splits = round(1/epoch_size)
+            match data_opts:
+                case None:
+                    return cycle([(X, y)])
+                case "partial":
+                    return cycle([(X[:split_size], y[:split_size])])
+                case "cycle":
+                    X_groups = array_split(X, n_splits)
+                    y_groups = array_split(y, n_splits)
+                    return cycle(zip(X_groups, y_groups))
+                case "window":
+                    ratio = (self.train_size-split_size) / ensemble_size
+                    data = zip(cycle(X), cycle(y))
+                    return windowed(data, n=split_size, step=(1 if ratio<1 else int(ratio)))
         
-        # split data for epochs < 1
-        if epochs >= 1:
-            data = zip([X], [y])
-        else:
-            n_splits = round(1/epochs)  # percent to number of groups
-            X_groups = array_split(X, n_splits)
-            y_groups = array_split(y, n_splits)
+        def train_weak_learner(X, y) -> skPerc:
+            return skPerc(max_iter=1).fit(X, y)
+        
+        def update_weights(new: skPerc) -> Tuple[ndarray, ndarray]:
+            self.base.coef_ = self.base.coef_ + new.coef_
+            self.base.intercept_ = self.base.intercept_ + new.intercept_
 
-            if cycle_groups:
-                self.cycle = True
-                data = zip(X_groups, y_groups)
-            else: # only use first group
-                self.cycle = False
-                data = zip([X_groups[0]], [y_groups[0]])
-                
         ## TRAINING
-        with Timer(initial_text="Training..."):
-            # init base to keep track of coef and intercept
-            base = skPerc()
-            base.coef_ = zeros(self.n_weights, self.n_features)
-            base.intercept_ = zeros(self.n_weights)
+        with Timer():
+            self.reset_base()
+            data = data_generator()
+            self.base.fit(*next(data))
             
-            for i, (X, y) in zip(range(ensemble_size), cycle(data)):
-                # train a perceptron once over the data
-                clf = skPerc(max_iter=1).fit(X, y)
-                coef, intercept = clf.coef_, clf.intercept_
-
-                # update base weights
-                base.coef_ = base.coef_ + coef
-                base.intercept_ = base.intercept_ + intercept
-                
-                # test
-                self.accuracies.append(self.test(base))
-                if log: print(i, ":", self.accuracies[-1])
+            for _ in range(ensemble_size - 1):
+                new_perc = train_weak_learner(*next(data))
+                update_weights(new_perc)
+                self.accuracies.append(self.test())
+                if log: print(self.accuracies[-1])
 
         # record
-        self.epochs = epochs
+        self.epoch_size = epoch_size
         self.ensemble_size = ensemble_size
+        self.data_opts = data_opts
         self.train_time = Timer().last
 
+    def plot(self):
+        plt.plot(self.accuracies)
+        plt.title(f"{self.dataset_name}")
+        plt.xlabel("Ensemble Size")
+        plt.ylabel("Accuracy")
+        plt.show()
 
 if __name__ == "__main__":
     ... 
